02dc99fdd485fdf0743d2039a9784b6de54f36c5
Tim Gates <tgates@iress.com.au>
2018-02-26 13:28:58 +1100

    Basic python docker file starting point

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3
+
+ADD hello.py /
+
+CMD [ "python", "./hello.py" ]
---
+++ hello.py
@@ -0,0 +1,15 @@
+"""
+Basic Commmand Line Entry Point
+"""
+
+from __future__ import absolute_import, print_function
+
+def main():
+    """
+    Main Command Line Handler
+    """
+    print('Hello World')
+
+if __name__ == '__main__':
+    main()
+
