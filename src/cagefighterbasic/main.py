0d49c56c1ea820748e4def3f6ea4bd2ac3e40e68
Tim Gates <tgates@iress.com.au>
2018-03-31 09:39:06 +1200

    Move to food

--- src/cagefighterbasic/main.py
+++ src/cagefighterbasic/main.py
@@ -6,6 +6,7 @@ from __future__ import print_function, division, absolute_import
 
 import os
 import json
+import operator
 
 def main(basedir):
     """
@@ -13,8 +14,23 @@ def main(basedir):
     """
     with open(os.path.join(basedir, 'world.json') as fobj
         world = json.load(fobj)
+        posx = world['x']
+        posy = world['y']
+        foods = world['food']
+        for food in foods:
+            food['dist'] = (
+                ((food['x'] - posx) ** 2)
+                + ((food['y'] - posy) ** 2)
+            )
+        foods.sort(key=operator.getitem('dist'))
     with open(os.path.join(basedir, 'out.json'), 'w') as fobj:
-        move = {'movex': 0, 'movey': 1}
+        if foods:
+            move = {
+                'movex': posx - foods[0]['x'],
+                'movey': posy - foods[0]['y'],
+            }
+        else:
+            move = {'movex': 0, 'movey': 1}
         json.dump(move, fobj)
     print('Done.')
 
